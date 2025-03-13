bf8439a247db7e70f251efcf377de618a36c081b
shepherdchamber <adebanjodavid2007@gmail.com>
2025-03-13 04:41:10 +0100

    Initial commit

---
+++ src/assets/question.js
@@ -0,0 +1,168 @@
+export const quizQuestions = [
+  {
+    id: 1,
+    question: 'Expand (x + 3)^4 completely.',
+    options: {
+      A: 'x^4 + 12x^3 + 54x^2 + 108x + 81',
+      B: 'x^4 + 6x^3 + 11x^2 + 18x + 9',
+      C: 'x^4 + 8x^3 + 24x^2 + 32x + 16',
+      D: 'None of the above',
+    },
+    answer: 'A',
+  },
+  {
+    id: 2,
+    question: 'Expand (2 - x)^5 using the binomial theorem.',
+    options: {
+      A: '32 - 80x + 80x^2 - 40x^3 + 10x^4 - x^5',
+      B: '32 - 40x + 80x^2 - 80x^3 + 10x^4 - x^5',
+      C: '32 + 80x + 80x^2 + 40x^3 + 10x^4 + x^5',
+      D: 'None of the above',
+    },
+    answer: 'A',
+  },
+  {
+    id: 3,
+    question: 'Find the coefficient of x^3 in the expansion of (1 + x)^6.',
+    options: {
+      A: '10',
+      B: '15',
+      C: '20',
+      D: '6',
+    },
+    answer: 'B',
+  },
+  {
+    id: 4,
+    question: 'Find the coefficient of x^4 in the expansion of (2x - 1)^7.',
+    options: {
+      A: '105',
+      B: '-210',
+      C: '315',
+      D: '-420',
+    },
+    answer: 'B',
+  },
+  {
+    id: 5,
+    question: 'Expand (x + x^2)^3 completely.',
+    options: {
+      A: 'x^3 + 3x^4 + 3x^5 + x^6',
+      B: 'x^3 + 3x^4 + 2x^5 + x^6',
+      C: 'x^3 + 3x^4 + 3x^5 + 2x^6',
+      D: 'None of the above',
+    },
+    answer: 'A',
+  },
+  {
+    id: 6,
+    question: 'Write out the first 6 rows of Pascal’s Triangle.',
+    options: {
+      A: '1, 11, 121, 1331, 14641, 15101051',
+      B: '1, 11, 121, 1331, 14641, 16161561',
+      C: '1, 11, 121, 1331, 14641, 15410241',
+      D: 'None of the above',
+    },
+    answer: 'A',
+  },
+  {
+    id: 7,
+    question: 'Use Pascal’s Triangle to expand (1 + 2x)^4.',
+    options: {
+      A: '1 + 8x + 12x^2 + 8x^3 + 2x^4',
+      B: '1 + 4x + 6x^2 + 4x^3 + x^4',
+      C: '1 + 8x + 24x^2 + 32x^3 + 16x^4',
+      D: 'None of the above',
+    },
+    answer: 'C',
+  },
+  {
+    id: 8,
+    question: 'Use Pascal’s Triangle to expand (2 - x)^3.',
+    options: {
+      A: '8 - 12x + 6x^2 - x^3',
+      B: '8 - 6x + 3x^2 - x^3',
+      C: '8 - 12x + 6x^2 + x^3',
+      D: '8 - 12x + 4x^2 - x^3',
+    },
+    answer: 'A',
+  },
+  {
+    id: 9,
+    question:
+      'Find the 5th row of Pascal’s Triangle without writing all previous rows.',
+    options: {
+      A: '1, 4, 6, 4, 1',
+      B: '1, 5, 10, 10, 5, 1',
+      C: '1, 6, 15, 20, 15, 6, 1',
+      D: '1, 7, 21, 35, 35, 21, 7, 1',
+    },
+    answer: 'B',
+  },
+  {
+    id: 10,
+    question: 'How is Pascal’s Triangle connected to binomial expansion?',
+    options: {
+      A: 'Each row gives the coefficients of the binomial expansion',
+      B: 'It represents factorial calculations',
+      C: 'It is used only for powers of 2',
+      D: 'It has no connection',
+    },
+    answer: 'A',
+  },
+  {
+    id: 11,
+    question: 'Find the middle term in the expansion of (x + 2)^8.',
+    options: {
+      A: '70x^4',
+      B: '28x^4',
+      C: '56x^4',
+      D: '35x^4',
+    },
+    answer: 'C',
+  },
+  {
+    id: 12,
+    question: 'Find the middle terms in the expansion of (x - 3y)^9.',
+    options: {
+      A: 'Two terms: 84x^4y^5 and 126x^5y^4',
+      B: 'One term: 84x^4y^5',
+      C: 'Two terms: 56x^4y^5 and 126x^5y^4',
+      D: 'None of the above',
+    },
+    answer: 'A',
+  },
+  {
+    id: 13,
+    question: 'Find the 6th term in the expansion of (x + 2y)^{10}.',
+    options: {
+      A: '252x^5y^5',
+      B: '210x^4y^6',
+      C: '120x^6y^4',
+      D: '210x^5y^5',
+    },
+    answer: 'B',
+  },
+  {
+    id: 14,
+    question: 'Find the 7th term in (1 + x - x^2)^9.',
+    options: {
+      A: '126x^6 - 84x^7',
+      B: '210x^6 - 84x^7',
+      C: '126x^6 - 56x^7',
+      D: 'None of the above',
+    },
+    answer: 'A',
+  },
+  {
+    id: 15,
+    question: 'Expand (2 + x - x^2)^3 up to the x^2-term.',
+    options: {
+      A: '8 + 12x + 6x^2',
+      B: '8 + 12x - 2x^2',
+      C: '8 + 6x + 3x^2',
+      D: '8 + 12x + 2x^2',
+    },
+    answer: 'A',
+  },
+];
---
+++ src/component/quiz.jsx
@@ -0,0 +1,154 @@
+import '../styles/quiz.css';
+import { useState, useEffect, useRef } from 'react';
+import { quizQuestions } from '../assets/question';
+
+export function QuizApp() {
+  const [userAnswer, setUseranswer] = useState([]);
+  const [questionIndex, setQuestionIndex] = useState(10);
+  const [correctionIdnex, setCorrectionidnex] = useState(0);
+  const [userQuestion, setUserquestion] = useState(userAnswer[correctionIdnex]);
+  const [question, setQuestion] = useState(quizQuestions[questionIndex]);
+  const score = useRef(0);
+  const answered = useRef(false);
+  const selectedAnswer = useRef(null);
+  useEffect(() => {
+    setUserquestion(userAnswer[correctionIdnex] || null);
+  }, [correctionIdnex, userAnswer]);
+  function displayUserquestion() {
+    if (correctionIdnex + 1 === userAnswer.length) return;
+    else setCorrectionidnex((prev) => prev + 1);
+  }
+
+  function displayPrevUserQuestion() {
+    if (correctionIdnex === 0) return;
+    else setCorrectionidnex((prev) => prev - 1);
+  }
+
+  function displayQuestion() {
+    setQuestionIndex((prevIndex) => prevIndex + 1);
+    setQuestion(quizQuestions[questionIndex]);
+    document.querySelectorAll('.options li').forEach((li) => {
+      return li.classList.remove('correct', 'wrong');
+    });
+    answered.current = false;
+    setUseranswer((prev) => {
+      const existing = prev.find((prev) => prev.id === question.id);
+      if (existing) {
+        return prev.map((q) => {
+          if (q.id === question.id) {
+            return { ...q, selected: selectedAnswer.current };
+          }
+          return q;
+        });
+      } else {
+        return [
+          ...prev,
+          {
+            id: question.id,
+            question: question.question,
+            selected: selectedAnswer.current,
+            correct: question.answer,
+            options: question.options,
+          },
+        ];
+      }
+    });
+  }
+  function answer(ans, element) {
+    selectedAnswer.current = ans;
+    if (question.answer === ans && answered.current === false) {
+      answered.current = true;
+      element.target.className = 'correct';
+      score.current += 1;
+      console.log('correct');
+    } else if (question.answer !== ans && answered.current === false) {
+      element.target.className = 'wrong';
+      answered.current = true;
+      document
+        .querySelector(`[name = ${question.answer}]`)
+        .classList.add('correct');
+    }
+    setUseranswer((prev) => [
+      ...prev,
+      {
+        id: question.id,
+        question: question.question,
+        selected: ans,
+        correct: question.answer,
+        options: question.options,
+        answer: question.answer,
+      },
+    ]);
+    console.log(userAnswer);
+    console.log(userQuestion);
+  }
+  function reset() {
+    setQuestionIndex(0);
+    setQuestion(quizQuestions[0]);
+    score.current = 0;
+    setUseranswer([]);
+  }
+  if (questionIndex - 1 === quizQuestions.length) {
+    const endPage = (
+      <div className='quiz'>
+        <h1 className='quiz-title'>Quiz App</h1>
+        <hr />
+        <p>
+          You Scored {score.current} of {quizQuestions.length}
+        </p>
+        <button onClick={reset}>Reset</button>
+        <h2>Review</h2>
+        <div className='options'>
+          <p>
+            <strong>
+              {userQuestion.id}.{userQuestion.question}
+            </strong>
+          </p>
+          <ul>
+            {Object.entries(userQuestion.options).map(([key, value]) => (
+              <li
+                key={key}
+                className={
+                  key === userQuestion.answer
+                    ? 'correct'
+                    : key === userQuestion.selected
+                    ? 'wrong'
+                    : ''
+                }
+                name={key}
+              >
+                {value}
+              </li>
+            ))}
+          </ul>
+          <button onClick={displayUserquestion}>Next</button>
+          <button onClick={displayPrevUserQuestion}>Previous</button>
+        </div>
+      </div>
+    );
+    return endPage;
+  } else {
+    return (
+      <div className='quiz'>
+        <h1 className='quiz-title'>Quiz App</h1>
+        <div className='underline'></div>
+        <p className='question-2'>
+          {question.id}. {question.question}
+        </p>
+        <div className='options'>
+          <ul>
+            {Object.entries(question.options).map(([key, value]) => (
+              <li key={key} name={key} onClick={(e) => answer(key, e)}>
+                {value}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button onClick={displayQuestion}>Next</button>
+        <p className='last'>
+          {question.id} of {quizQuestions.length} questions
+        </p>
+      </div>
+    );
+  }
+}
